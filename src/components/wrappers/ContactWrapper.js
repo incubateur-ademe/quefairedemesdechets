1b346aff6aab846676f7587ab113dd59f2a5aa23
Florian <hi@florianpanchout.com>
2021-07-27 14:13:05 +0200

    Issue 10

--- src/components/wrappers/ContactWrapper.js
+++ src/components/wrappers/ContactWrapper.js
@@ -8,9 +8,12 @@ export default function ContactWrapper(props) {
       small={props.small}
       options={[
         {
-          value: 'Imprecision',
-          label: `Le calcul n'est pas assez précis`,
-          disclaimer: `Ce simulateur propose un calcul simplifié afin de donner une idée d'ordre de grandeur. Réalisez votre bilan carbone sur Nos Gestes Climat afin d'obtenir un résultat plus précis.`,
+          value: 'Erreur',
+          label: `Je souhaite signaler une erreur pour un déchet`,
+        },
+        {
+          value: 'Manquant',
+          label: `Je souhaite signaler un déchet manquant`,
         },
       ]}
     />
