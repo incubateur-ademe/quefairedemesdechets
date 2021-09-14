8c49f079259a29a3dcba4a782e5b93b059199a02
Florian <hi@florianpanchout.com>
2021-09-14 19:04:56 +0200

    Cors Fix

--- netlify/functions/callOcad3e.js
+++ netlify/functions/callOcad3e.js
@@ -3,11 +3,15 @@ const axios = require('axios')
 exports.handler = function (event) {
   return axios
     .get(
-      `https://www.bddpdcrep.fr/webservice.php?key=${process.env.OCAD3E_API_KEY}&method=getPlacemarks&format=json&lat=${event.queryStringParameters.latitude}&lng=${event.queryStringParameters.longitude}&wasteCategories[]=${event.queryStringParameters.category}`
+      `https://www.bddpdcrep.fr/webservice.php?key=${process.env.OCAD3E_API_KEY}&method=getPlacemarks&lat=${event.queryStringParameters.latitude}&lng=${event.queryStringParameters.longitude}&wasteCategories[]=${event.queryStringParameters.category}`
     )
     .then((res) => ({
       statusCode: 200,
-      header: JSON.stringify({ 'Access-Control-Allow-Origin': '*' }),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTION',
+      },
       body: JSON.stringify(res.data),
     }))
 }
