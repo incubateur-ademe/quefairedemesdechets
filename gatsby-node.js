b6cee399c8d85dfa586b9abfc0b1927d8942e84b
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-07-01 15:09:21 +0200

    add csv file + function to load it during build

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,17 +1,44 @@
 const axios = require(`axios`);
+const fs = require("fs");
+const { parse } = require("csv-parse");
 
-exports.createPages = ({ graphql, actions: { createPage } }) => {
+function updateKoumoulDataFrom(product, localData) {
+  const productInLocalData = localData.find(
+    (localProduct) => product.Code === localProduct[1]
+  );
+
+  if (productInLocalData) {
+    return {
+      ...product,
+      lvao: {
+        subCategoryID: productInLocalData[0],
+        mapUrl: productInLocalData[3],
+        description: productInLocalData[2],
+      },
+    };
+  }
+  return product;
+}
+
+exports.createPages = ({ actions: { createPage } }) => {
+  const localData = [];
+  fs.createReadStream("./data-config.csv")
+    .pipe(parse({ delimiter: ",", from_line: 2 }))
+    .on("data", function (row) {
+      localData.push(row);
+    });
+
   return axios
     .get(
       `https://data.ademe.fr/data-fair/api/v1/datasets/que-faire-de-mes-dechets-produits/lines?format=json&q_mode=simple&size=1000&sampling=neighbors`,
     )
     .then((res) =>
-      res.data.results.filter((waste) => typeof waste["ID"] !== "undefined"),
+      res.data.results.filter((waste) => typeof waste["ID"] !== "undefined")
     ) // handle Koumoul missing ID
     .then((wasteRes) =>
       axios
         .get(
-          "https://data.ademe.fr/data-fair/api/v1/datasets/que-faire-de-mes-dechets-liens/lines?format=json&q_mode=simple&size=1000&sampling=neighbors",
+          "https://data.ademe.fr/data-fair/api/v1/datasets/que-faire-de-mes-dechets-liens/lines?format=json&q_mode=simple&size=1000&sampling=neighbors"
         )
         .then((res) => res.data.results)
         .then((linkRes) => {
@@ -49,18 +76,18 @@ exports.createPages = ({ graphql, actions: { createPage } }) => {
             links: linkRes.filter((link) =>
               link["Produits_associes"]
                 .split("; ")
-                .includes(waste["ID"].split("_")[0]),
+                .includes(waste["ID"].split("_")[0])
             ),
           }));
-        }),
+        })
     )
     .then((res) =>
       res.forEach((product) => {
         createPage({
           path: `/dechet/${product.slug}/`,
           component: require.resolve("./src/templates/product.js"),
-          context: { product },
+          context: { product: updateKoumoulDataFrom(product, localData) },
         });
-      }),
+      })
     );
 };
