2dd6b17967359f27b0e7faf5f5a4b7e729d8b2b0
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-07-15 15:41:12 +0200

    rephrase comments and test title

--- tests/Accordion.test.tsx
+++ tests/Accordion.test.tsx
@@ -4,12 +4,11 @@ import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
 import Accordion from '../src/components/base/Accordion'
 
-test('display an accordion', async () => {
+test('Accordion : un clic sur le titre affiche le contenu', async () => {
   render(
     <Accordion items={[{ title: "Je suis un professionnel", content: "Actuellement, l’ensemble des recommandations ne concerne que les particuliers. " }]} />
   )
 
-  // ACT
   expect(screen.getByTestId('accordion-content')).not.toBeVisible()
   await userEvent.click(screen.getByTestId('accordion-button'))
   expect(screen.getByTestId('accordion-content')).toBeVisible()
