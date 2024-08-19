5f41f9fd79d6e1005a9eadaac175c423d310ed8f
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-08-19 17:58:51 +0200

    Allow react element or string

--- src/views/home/ProDisclaimer.tsx
+++ src/views/home/ProDisclaimer.tsx
@@ -9,7 +9,7 @@ const Wrapper = styled.div`
 export default function ProDisclaimer() {
   return (
     <Wrapper>
-      <Accordion items={[{ title: "Je suis un professionnel", content: "Actuellement, l’ensemble des recommandations ne concerne que les particuliers. Pour des informations à destination des professionnels, veuillez consulter le site <a href="https://economie-circulaire.ademe.fr/dechets-activites-economiques" target="_blank">https://economie-circulaire.ademe.fr/dechets-activites-economiques</a>." }]} />
+      <Accordion items={[{ title: "Je suis un professionnel", content: <p>Actuellement, l’ensemble des recommandations ne concerne que les particuliers. Pour des informations à destination des professionnels, veuillez consulter le site <a href="https://economie-circulaire.ademe.fr/dechets-activites-economiques" target="_blank">https://economie-circulaire.ademe.fr/dechets-activites-economiques</a>.</p> }]} />
     </Wrapper>
   );
 }
