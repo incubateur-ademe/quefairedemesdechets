6ec52242e7ab900394b6d7756e1041ab1eda56e5
Lucas Jouin <138494307+LuluFreeDesign@users.noreply.github.com>
2024-08-19 15:05:13 +0200

    Update Web.js

--- src/components/layout/Web.js
+++ src/components/layout/Web.js
@@ -35,7 +35,7 @@ const FullScreen = styled.div`
   flex: 1;
   display: flex;
   flex-direction: column;
-  width: 65rem;
+  width: 67rem;
   max-width: 100%;
   min-height: ${(props) => (props.iframe ? "none" : "100vh")};
   margin: 0 auto;
