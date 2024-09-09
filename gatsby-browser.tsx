940c8926a63095c876dc1995b456f3790f000ed3
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-09-09 13:13:54 +0200

    remove useless variables

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import StyleProvider from "./src/components/providers/StyleProvider";
 import ReactQueryProvider from "./src/components/providers/ReactQueryProvider";
 
